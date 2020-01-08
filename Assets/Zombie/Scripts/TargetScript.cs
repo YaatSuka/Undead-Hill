05b260e75e5499fb0e5ee82cc4b82bc96a18b78a
Nohan Palasse <nohan.palasse@epitech.eu>
2020-01-08 16:50:37 +0100

    terrain and zombie hit

---
+++ Assets/Zombie/Scripts/TargetScript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// ----- Low Poly FPS Pack Free Version -----
+public class TargetScript : MonoBehaviour {
+	//Used to check if the target has been hit
+	public bool isHit = false;
+	public int destroyDelay = 5;
+	
+	private void Update () {
+		//If the target is hit
+		if (isHit == true) 
+		{
+			gameObject.GetComponent<Animator>().Play("Z_FallingBack");
+			Destroy(gameObject, destroyDelay);
+		}
+	}
+}
+// ----- Low Poly FPS Pack Free Version -----
