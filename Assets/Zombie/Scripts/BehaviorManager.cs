80ec7cd8c305dae3f51f220a09e1734d8fb05f7a
Tom Chitty <tom.chitty@epitech.eu>
2020-01-08 18:36:59 +0100

    manage zombie animations + zombie follow

---
+++ Assets/Zombie/Scripts/BehaviorManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnitySteer.Behaviors;
+
+public class BehaviorManager : MonoBehaviour
+{
+    public float runDistance = 15f;
+    public float hitDistance = 2f;
+    private float distance;
+    private GameObject player;
+    private Animator animator;
+    private Biped bipedComponent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindWithTag("Player");
+        animator = gameObject.GetComponent<Animator>();
+        bipedComponent = gameObject.GetComponent<Biped>();
+        if (player) {
+            distance = Vector3.Distance(player.transform.position, transform.position);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        distance = Vector3.Distance(player.transform.position, transform.position);
+        if (distance <= runDistance && distance > hitDistance) {
+            animator.Play("Z_Run_InPlace");
+            bipedComponent.MaxSpeed = 3;
+        } else if (distance <= hitDistance) {
+            animator.Play("Z_Attack");
+        } else {
+            animator.Play("Z_Walk_InPlace");
+            bipedComponent.MaxSpeed = 1;
+        }
+
+        //Debug.Log(distance);
+    }
+}
--- Assets/Zombie/Scripts/TargetScript.cs
+++ Assets/Zombie/Scripts/TargetScript.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnitySteer.Behaviors;
 
 // ----- Low Poly FPS Pack Free Version -----
 public class TargetScript : MonoBehaviour {
@@ -12,6 +13,9 @@ public class TargetScript : MonoBehaviour {
 		if (isHit == true) 
 		{
 			gameObject.GetComponent<Animator>().Play("Z_FallingBack");
+			gameObject.GetComponent<SteerToFollow>().enabled = false;
+			gameObject.GetComponent<BehaviorManager>().enabled = false;
+			gameObject.GetComponent<Biped>().enabled = false;
 			Destroy(gameObject, destroyDelay);
 		}
 	}
