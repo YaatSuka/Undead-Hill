501fa6c816bf1412966d89130f1993f3edb10402
Tom Chitty <tom.chitty@epitech.eu>
2020-01-09 14:53:45 +0100

    increase spawn ratio every minute

--- Assets/Zombie/Scripts/SpawnZombie.cs
+++ Assets/Zombie/Scripts/SpawnZombie.cs
@@ -6,27 +6,33 @@ using UnitySteer.Behaviors;
 public class SpawnZombie : MonoBehaviour
 {
     public GameObject zombiePrefab;
+    public float spawnRatio = 10f;
     private GameObject player;
     private IEnumerator coroutine;
+    private float timer = 0f;
 
     // Start is called before the first frame update
     void Start()
     {
         player = GameObject.FindWithTag("Player");
-        coroutine = Spawn(5.0f);
+        coroutine = Spawn();
         StartCoroutine(coroutine);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        timer += Time.deltaTime;
+        if (timer > 60f && spawnRatio > 3f) {
+            spawnRatio -= 0.5f;
+            timer = 0;
+        }
     }
 
-    private IEnumerator Spawn(float waitTime)
+    private IEnumerator Spawn()
     {
         while (true) {
-            yield return new WaitForSeconds(waitTime);
+            yield return new WaitForSeconds(spawnRatio);
             GameObject zombie = Instantiate(zombiePrefab, transform.position, Quaternion.identity);
             zombie.GetComponent<SteerToFollow>().Target = player.transform;
         }
